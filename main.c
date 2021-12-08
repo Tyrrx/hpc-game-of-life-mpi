b687331eb19d5feec84f4b7ef17e488d1ca0eee7
Tyrrx <david.r3tzlaff@gmail.com>
2021-12-08 21:01:47 +0100

    try fix

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 #define calcIndex(width, x, y)  ((y)*(width) + (x))
 
-void printGrid(int rank, struct Vec2i *grid_size, const int *field_buffer);
+void printGrid(int rank, struct Vec2i grid_size, const int *field_buffer);
 
 void initGrid(int rank, struct Vec2i grid_size, int *field_buffer);
 
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
 
 
     // define the local gird sizes
-    struct Vec2i small = new_vec2i(25, 25);
+    struct Vec2i small = new_vec2i(5, 5);
 
     // calc the local grid size with ghosts
     struct Vec2i large = add(small, new_vec2i(2, 0));
@@ -131,14 +131,14 @@ int main(int argc, char *argv[])
     int *field_buffer_swap = calloc(large_size, sizeof(int));
 
 
-    //initGrid(rank, small, field_buffer);
+    initGrid(rank, small, field_buffer);
 
 
     // create the kernel
     const struct Kernel2d kernel2d = create_kernel();
     const struct Kernel1d kernel = translate_kernel_1d(large, kernel2d);
 
-    if(rank == 0) {
+    if (rank == 0) {
         field_buffer[large.x1 + 3] = 1;
         field_buffer[large.x1 * 2 + 3] = 1;
         field_buffer[large.x1 * 3 + 3] = 1;
@@ -147,7 +147,7 @@ int main(int argc, char *argv[])
     }
 
     // do the steps
-    for (int step = 0; step < 200; ++step) {
+    for (int step = 0; step < 4; ++step) {
         // define buffers that track the changes (spawn/die)
         int local_changes = 0;
         int global_changes = 0;
@@ -157,72 +157,72 @@ int main(int argc, char *argv[])
         // todo use ISend and IRecv
 
         // get coords in topology
-        int coords[1];
-        MPI_Cart_coords(communicator, rank, 1, coords);
-        // calculate the absolute position inside the grid
-        struct Vec2i absolute_origin = multiply(small, new_vec2i(coords[0], 0));
-        writeVTK2(step, field_buffer, "golmpi", rank, absolute_origin, small, large);
+//        int coords[1];
+//        MPI_Cart_coords(communicator, rank, 1, coords);
+//        // calculate the absolute position inside the grid
+//        struct Vec2i absolute_origin = multiply(small, new_vec2i(coords[0], 0));
+//        writeVTK2(step, field_buffer, "golmpi", rank, absolute_origin, small, large);
 
         // exchange ghost layers
         MPI_Neighbor_alltoallw(field_buffer, send_counts, send_dsp, send_types, field_buffer, rec_counts, rec_dsp,
                                rec_types, communicator);
-
-        // iterate over y first for cache alignment
-        for (int y = 0; y < small.x2; ++y) {
-
-            // calculate the y0 position inside the 1d array
-            const int iy = large.x2 * y;
-
-            for (int x = 0; x < small.x1; ++x) {
-                int surroundings = 0;
-
-                // shift the x position by one and add to y0
-                const int ixy = iy + x + 1;
-
-                // count the surroundings
-                for (int i = 0; i < kernel.num_elements; ++i) {
-                    const int ixy_k = ixy + kernel.elements[i];
-                    if (field_buffer[(ixy_k + large_size) % large_size] == 1) {
-                        surroundings++;
-                    }
-                }
-
-                // copy the old value
-                field_buffer_swap[ixy] = field_buffer[ixy];
-
-                const int alive = field_buffer[ixy];
-                // ------------------------------------- spawn = changes
-                if (!alive && surroundings == 3) {
-                    field_buffer_swap[ixy] = 1;
-                    local_changes = 1;
-                }
-                // do nothing?
-                if (alive && (surroundings == 2 || surroundings == 3)) {
-                    field_buffer_swap[ixy] = 1;
-                }
-                // ------------------------------------- die = changes
-                if (alive && (surroundings == 0 || surroundings == 1 || surroundings > 3)) {
-                    field_buffer_swap[ixy] = 0;
-                    local_changes = 1;
-                }
-
-            }
-        }
-
-        // swap buffers
-        int *temp = field_buffer;
-        field_buffer = field_buffer_swap;
-        field_buffer_swap = temp;
-
-        // distribute local changes to all ranks and exit if none
-        // todo works as a barrier?
-        MPI_Barrier(communicator);
-        MPI_Allreduce(&local_changes, &global_changes, 1, MPI_INT, MPI_SUM, communicator);
-        if (global_changes == 0) {
-            printf("Rank:%d Step:%d exited with no changes\n", rank, step);
-            break;
-        }
-        //printGrid(rank, &small, field_buffer);
+//
+//        // iterate over y first for cache alignment
+//        for (int y = 0; y < small.x2; ++y) {
+//
+//            // calculate the y0 position inside the 1d array
+//            const int iy = large.x2 * y;
+//
+//            for (int x = 0; x < small.x1; ++x) {
+//                int surroundings = 0;
+//
+//                // shift the x position by one and add to y0
+//                const int ixy = iy + x + 1;
+//
+//                // count the surroundings
+//                for (int i = 0; i < kernel.num_elements; ++i) {
+//                    const int ixy_k = ixy + kernel.elements[i];
+//                    if (field_buffer[(ixy_k + large_size) % large_size] == 1) {
+//                        surroundings++;
+//                    }
+//                }
+//
+//                // copy the old value
+//                field_buffer_swap[ixy] = field_buffer[ixy];
+//
+//                const int alive = field_buffer[ixy];
+//                // ------------------------------------- spawn = changes
+//                if (!alive && surroundings == 3) {
+//                    field_buffer_swap[ixy] = 1;
+//                    local_changes = 1;
+//                }
+//                // do nothing?
+//                if (alive && (surroundings == 2 || surroundings == 3)) {
+//                    field_buffer_swap[ixy] = 1;
+//                }
+//                // ------------------------------------- die = changes
+//                if (alive && (surroundings == 0 || surroundings == 1 || surroundings > 3)) {
+//                    field_buffer_swap[ixy] = 0;
+//                    local_changes = 1;
+//                }
+//
+//            }
+//        }
+//
+//        // swap buffers
+//        int *temp = field_buffer;
+//        field_buffer = field_buffer_swap;
+//        field_buffer_swap = temp;
+//
+//        // distribute local changes to all ranks and exit if none
+//        // todo works as a barrier?
+//        MPI_Barrier(communicator);
+//        MPI_Allreduce(&local_changes, &global_changes, 1, MPI_INT, MPI_SUM, communicator);
+//        if (global_changes == 0) {
+//            printf("Rank:%d Step:%d exited with no changes\n", rank, step);
+//            break;
+//        }
+        printGrid(rank, small, field_buffer);
 
     }
 
@@ -242,20 +242,20 @@ void initGrid(int rank, struct Vec2i grid_size, int *field_buffer)
 {
     for (int y = 0; y < grid_size.x2; ++y) {
         for (int x = 1; x < grid_size.x1 + 1; ++x) {
-            if (y == x - 1) {
-                field_buffer[calcIndex(grid_size.x1, x, y)] = 1;
-            }
+
+            field_buffer[calcIndex(grid_size.x1, x, y)] = 1;
+
         }
     }
 }
 
-void printGrid(int rank, struct Vec2i *grid_size, const int *field_buffer)
+void printGrid(int rank, struct Vec2i grid_size, const int *field_buffer)
 {
     char out[2000], *put = out;
     put += sprintf(put, "R%d-----------------------------------------------\n", rank);
-    for (int y = 0; y < (*grid_size).x2; ++y) {
-        for (int x = 0; x < (*grid_size).x1 + 2; ++x) {
-            put += sprintf(put, "%d", field_buffer[calcIndex((*grid_size).x1 + 2, x, y)]);
+    for (int y = 0; y < grid_size.x2; ++y) {
+        for (int x = 1; x < grid_size.x1 + 1; ++x) {
+            put += sprintf(put, "%d", field_buffer[calcIndex(grid_size.x1 + 2, x, y)]);
         }
         put += sprintf(put, "\n");
     }
